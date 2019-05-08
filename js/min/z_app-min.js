f45a1fd4064ae0418bc8272849bab1c124501fed
Adam Keller <akeller@cardinalsolutions.com>
2019-05-08 08:15:57 -0400

    New Start

--- js/min/z_app-min.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$(".icon").click(function(){$("body").toggleClass("no-scroll"),$(".web-frame").toggleClass("no-scroll"),$(".mobilenav").fadeToggle(500),$(".top-menu").toggleClass("top-animate"),$(".mid-menu").toggleClass("mid-animate"),$(".bottom-menu").toggleClass("bottom-animate")})});
